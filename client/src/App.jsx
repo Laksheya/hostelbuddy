6180cbaeeff813037838f859373558e7110338e7
Aditya Singh <aditya.singh@testrigor.com>
2024-07-27 15:41:24 +0530

    added signup form
    kllk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@material-tailwind/react";
+import SignUpForm from './pages/signUpForm';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="App">
+      <SignUpForm />
+    </div>
       <div>
       <Button>Button</Button>
 
